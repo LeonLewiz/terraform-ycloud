add0fd93af493c02bbac968d99790afedddc5164
LeonLewiz <62465978+LeonLewiz@users.noreply.github.com>
2026-09-15 23:13:16 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,8 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
-}}
+  }
+}
 provider "yandex" {
   service_account_key_file     = "key.json"
 
