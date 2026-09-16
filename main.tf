fd69e76522aaf2137fba8da3226e1da11fe94979
LeonLewiz <62465978+LeonLewiz@users.noreply.github.com>
2026-09-16 19:10:46 +0400

    Change Ubuntu image family in main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ data "yandex_vpc_subnet" "default_a" {
 }
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2204-lts"
+  family = "leonlewiz-study-family"
 }
 
 resource "tls_private_key" "ssh_key" {
