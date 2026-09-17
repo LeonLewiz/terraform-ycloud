9461777a0c8e6b7c31af799e6c1ec9ff0cc3c789
LeonLewiz <62465978+LeonLewiz@users.noreply.github.com>
2026-09-17 19:44:44 +0400

    Add logging, monitoring IAM members and worker node group
    Added resources for logging and monitoring IAM members, and created a worker node group with specific configurations.

--- k8s.tf
+++ k8s.tf
@@ -1,3 +1,7 @@
+data "yandex_vpc_network" "default" {
+  name = "default"
+}
+
 resource "yandex_vpc_security_group" "k8s" {
   name       = "k8s-sg"
   network_id = data.yandex_vpc_network.default.id
@@ -8,14 +12,6 @@ resource "yandex_vpc_security_group" "k8s" {
     port           = 6443
     v4_cidr_blocks = ["192.168.13.130/32"]
   }
-
-  ingress {
-    description    = "ssh"
-    protocol       = "TCP"
-    port           = 22
-    v4_cidr_blocks = ["192.168.13.130/32"]
-  }
-
 
   ingress {
     description       = "selfin"
@@ -38,10 +34,6 @@ resource "yandex_vpc_security_group" "k8s" {
     from_port         = 0
     to_port           = 65535
   }
-}
-
-data "yandex_vpc_network" "default" {
-  name = "default"
 }
 
 resource "yandex_iam_service_account" "k8s" {
@@ -84,6 +76,18 @@ resource "yandex_resourcemanager_folder_iam_member" "k8s_encrypterdecrypter" {
   member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
 }
 
+resource "yandex_resourcemanager_folder_iam_member" "k8s_logging" {
+  folder_id = var.folder_id
+  role      = "logging.writer"
+  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "k8s_monitoring" {
+  folder_id = var.folder_id
+  role      = "monitoring.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.k8s.id}"
+}
+
 resource "yandex_kubernetes_cluster" "k8s" {
   name       = "k8s-cluster"
   network_id = data.yandex_vpc_network.default.id
@@ -114,5 +118,67 @@ resource "yandex_kubernetes_cluster" "k8s" {
     yandex_resourcemanager_folder_iam_member.k8s_lb,
     yandex_resourcemanager_folder_iam_member.k8s_tunnel,
     yandex_resourcemanager_folder_iam_member.k8s_encrypterdecrypter
+    yandex_resourcemanager_folder_iam_member.k8s_logging
+    yandex_resourcemanager_folder_iam_member.k8s_monitoring
+  ]
+}
+
+resource "yandex_kubernetes_node_group" "worker" {
+  cluster_id = yandex_kubernetes_cluster.k8s.id
+  name       = "k8sworker"
+
+  instance_template {
+    platform_id = "standard-v3"
+
+    resources {
+      cores         = 2
+      memory        = 2
+      core_fraction = 20
+    }
+
+    boot_disk {
+      type = "network-hdd"
+      size = 64
+    }
+
+    network_interface {
+      subnet_ids         = [data.yandex_vpc_subnet.default_a.id]
+      nat                = true
+      security_group_ids = [yandex_vpc_security_group.k8s.id]
+    }
+
+    container_runtime {
+      type = "containerd"
+    }
+  }
+
+  node_service_account_id = yandex_iam_service_account.k8s.id
+
+  scale_policy {
+    fixed_scale {
+      size = 1
+    }
+  }
+
+  allocation_policy {
+    location {
+      zone = var.zone
+    }
+  }
+
+  maintenance_policy {
+    auto_upgrade = false
+    auto_repair  = true
+  }
+
+  depends_on = [
+    yandex_resourcemanager_folder_iam_member.k8s_agent,
+    yandex_resourcemanager_folder_iam_member.k8s_puller,
+    yandex_resourcemanager_folder_iam_member.k8s_vpc,
+    yandex_resourcemanager_folder_iam_member.k8s_lb,
+    yandex_resourcemanager_folder_iam_member.k8s_tunnel,
+    yandex_resourcemanager_folder_iam_member.k8s_encrypterdecrypter
+    yandex_resourcemanager_folder_iam_member.k8s_logging
+    yandex_resourcemanager_folder_iam_member.k8s_monitoring
   ]
 }
